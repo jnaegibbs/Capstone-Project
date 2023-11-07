241052d28a7860b15cb9e37416dcc52adfbcc7e3
Divya Praveen <divyapraveen@Divyas-MacBook-Air.local>
2023-11-06 22:24:58 -0600

    added post category

--- server/api/pets.js
+++ server/api/pets.js
@@ -1,31 +1,43 @@
 const router = require("express").Router();
 
-const prisma = require('../db/client');
+const prisma = require("../db/client");
 
-
+// GET /api/pets
 router.get("/", async (req, res, next) => {
-    try {
-      const pets = prisma.category.findMany();
-      res.send({pets});
-    } catch (error) {
-      next(error);
-    }
-  });
+  try {
+    const pets = await prisma.category.findMany();
 
-  router.post("/", async (req, res, next) => {
-    try {
-      const pet = await prisma.category.create({
-        data:{
-          products: req.body.product,
-          petsName:req.body.petsName
-        }
-      });
-      console.log(pet);
-      res.status(201).send({pet});
-    } catch (error) {
-      next(error);
-    }
-  });
+    res.send({ pets });
+  } catch ({name,message}) {
+    next({name,message});
+  }
+});
 
- 
-  module.exports = router;
+// POST /api/pets
+router.post("/", async (req, res, next) => {
+  try {
+    const newCategory = await prisma.category.create({
+      data: {
+        productType: req.body.productType,
+        products: {
+          create: {
+            name: req.body.name,
+            image: req.body.image,
+            price: req.body.price,
+            description: req.body.description,
+            productCount: req.body.productCount,
+            petCategory: req.body.petCategory,
+          },
+        },
+        petCategory: req.body.petCategory,
+       
+      },
+    });
+
+    res.status(201).send({ newCategory});
+  } catch ({name,message}) {
+    next({name,message});
+  }
+});
+
+module.exports = router;
