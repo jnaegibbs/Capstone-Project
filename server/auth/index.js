681adef351810a4611553ff74f77101543a08370
J’nae Gibbs <jnaegibbs@Jnaes-Air.lan>
2023-11-08 22:09:38 -0500

    register, login, auth middleware

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,4 +1,38 @@
 const authRouter = require("express").Router();
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = process.env
+const prisma = require("../db/client");
+
+authRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+    if (!auth) {
+      next();
+    } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+      try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+        if (id) {
+          req.user = await prisma.user.findUnique({
+            where: {id: id}
+          });
+          next();
+        } else {
+          next({
+            name: 'AuthorizationHeaderError',
+            message: 'Authorization token malformed',
+          });
+        }
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    } else {
+      next({
+        name: 'AuthorizationHeaderError',
+        message: `Authorization token must start with ${prefix}`,
+      });
+    }
+  });
 
 authRouter.use("/user", require('./user'));
 authRouter.use("/admin", require("./admin"));
--- server/auth/user.js
+++ server/auth/user.js
@@ -1,8 +1,59 @@
 const userRouter = require("express").Router();
 
 const prisma = require("../db/client");
+const jwt = require('jsonwebtoken');
+const SALT_COUNT = 10;
+const bcrypt = require("bcrypt");
+const {JWT_SECRET} = process.env;
 
-//Todo : write api here
+userRouter.get('/', async (req, res, next) => {
+    try {
+        const allUsers = await prisma.user.findMany();
+        res.send(allUsers);
+        console.log(allUsers);
+    } catch (error) {
+        next(error);
+    }
+});
 
+userRouter.post('/register', async (req, res, next) => {
+    try {
+        const {username, name, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+        const user = await prisma.user.create({
+            data: {
+                username, 
+                password: hashedPassword,
+                name
+            }
+        });
+        const token = jwt.sign({id: user.id}, JWT_SECRET);
+        res.status(201).send({user, token});
+    } catch (error) {
+        next(error);
+    }
+});
+
+userRouter.post('/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({
+          where: {
+            username: username
+          }
+        })
+        if(!user) {
+          return res.status(404).json({message: "User not found"});
+        }
+        const passwordValid = await bcrypt.compare(password, user.password)
+        if(!passwordValid) {
+          return res.status(201).json({message: "Invalid password"})
+        }
+        const token = jwt.sign({id: user.id}, JWT_SECRET);
+        res.status(200).json({user, token})
+      } catch(error){
+        next(error)
+      }
+})
 
 module.exports = userRouter;
