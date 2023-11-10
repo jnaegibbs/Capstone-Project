51163f17e071fe201f701074c7bc65d00203ff9b
Katie Barrett <kayt.barrett22@gmail.com>
2023-11-09 18:30:53 -0700

    orders updated

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,14 +1,14 @@
 function requireUser(req, res, next) {
     if (!req.user) {
-      res.status(401);
-      next({
-        name: "MissingUserError",
-        message: "You must be logged in to perform this action"
-      });
+        res.status(401).json({
+            name: "MissingUserError",
+            message: "You must be logged in to perform this action"
+        });
+    } else {
+        console.log('User authenticated');
+        next();
     }
-  
-    next();
-  }
+}
   
   module.exports = {
     requireUser
