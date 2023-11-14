c163e7bc0b8e6f3fdafb3103f6184b984184fc7c
divya-prav <divya.praveen.5215@gmail.com>
2023-11-14 14:26:47 -0600

    Added: rtk query for orderApi

---
+++ client/redux/orderApi.js
@@ -0,0 +1,77 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const orderApi = createApi({
+  reducerPath: "orderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/",
+  }),
+
+  endpoints: (builder) => ({
+    //get all the orders
+    getOrders: builder.query({
+      query: () => `api/pets/order`,
+    }),
+     // get a single order
+    getSingleorder: builder.query({
+      query: (orderId) => `api/pets/order/${orderId}`,
+    }),
+
+    //create a new order
+    addOrder: builder.mutation({
+      query: (orderDetails) => ({
+        url: `api/pets/order`,
+        method: "POST",
+        body: orderDetails,
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    
+    }),
+
+    //update the existed order
+    updateOrder: builder.mutation({
+      query: (orderId, ...orderDetails) => ({
+        url: `api/pets/order/${orderId}`,
+        method: "PUT",
+        body: orderDetails,
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    }),
+
+    //delete the existed order
+    deleteOrder: builder.mutation({
+      query: (orderId) => ({
+        url: `api/pets/order/${orderId}`,
+        method: "DELETE",
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    }),
+  }),
+});
+
+export default orderApi;
+export const {
+  useGetOrdersQuery,
+  useGetSingleorderQuery,
+  useAddOrderMutation,
+  useUpdateOrderMutation,
+  useDeleteOrderMutation,
+} = orderApi;
