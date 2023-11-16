b5235b8f3d79a0514b9e6b3f25f3da3a966e0eee
divya-prav <divya.praveen.5215@gmail.com>
2023-11-16 09:00:31 -0600

    navbar undone

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,52 +1,85 @@
-import { Typography, Container, Toolbar, IconButton } from "@mui/material";
+import { Typography, Container, Toolbar, IconButton, Box } from "@mui/material";
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import { FaPaw } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate }  from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 const NavBar = () => {
   return (
     <>
-      <AppBar position="sticky" color="primary">
-        <Container maxWidth="xl">
-          <Toolbar>
-            <div style={{ display: "inline-block", marginRight: "10px" }}>
-              {" "}
-              <FaPaw sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}  />
-            </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="sticky" color="primary">
+          <Container maxWidth="xl">
+            <Toolbar>
+              <IconButton color="inherit" size="large">
+                <FaPaw />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component="div"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  fontSize: "large",
+                  letterSpacing: ".2rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                FLUFFY FRIENDS
+              </Typography>
 
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                fontSize: "large",
-                letterSpacing: ".2rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              FLUFFY FRIENDS
-            </Typography>
+              <Typography
+                variant="h6"
+                component="div"
+                href="#app-bar-with-responsive-menu"
+                style={{ marginLeft: "50%" }}
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  fontSize: "large",
+                  letterSpacing: ".1rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Signup
+              </Typography>
 
-            <IconButton color="inherit">
-                Sign up
-            </IconButton>
-            <IconButton color="inherit">
-              Orders 
-            </IconButton>
-            <IconButton color="inherit">
-              <FaShoppingCart />
-            </IconButton>
-           
-          </Toolbar>
-        </Container>
-      </AppBar>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  fontSize: "large",
+                  letterSpacing: ".1rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Orders
+              </Typography>
+
+              <IconButton color="inherit">
+                <FaShoppingCart />
+              </IconButton>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
     </>
   );
 };
