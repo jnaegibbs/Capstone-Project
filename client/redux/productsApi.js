540f053ad4f51e7ccdbf82a0255726309670b7b3
Mari <dennismaricarl@gmail.com>
2023-11-15 18:12:56 -0800

    productsApi created & separated from petsApi.js

---
+++ client/redux/productsApi.js
@@ -0,0 +1,77 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const productsApi = createApi ({
+    reducerPath: "productsApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:8080"
+    }),
+
+    endpoints: (builder) => ({
+        //get all products
+        fetchProducts: builder.query({
+            query: () => `api/pets/product`,
+        }),
+
+        //get single product
+        fetchSingleProduct: builder.query({
+            query: (productId) => `api/pets/product/${productId}`
+        }),
+
+        //create a new product 
+        newProduct: builder.mutation({
+            query: (newProduct) => ({
+                url: `api/pets/product`,
+                method: "POST",
+                body: newProduct,
+            }),
+            transformResponse: (response) => {
+                console.log(response);
+                return(response);
+            },
+            transformErrorResponse: (response) => {
+                console.log(response.status)
+            },
+        }),
+
+        //update existing product
+        updateProduct: builder.mutation({
+            query:(productId, updateProduct) => ({
+                url:`api/pets/product/${productId}`,
+                method: "POST",
+                body: updateProduct
+            }),
+            transformResponse: (response) => {
+                console.log(response);
+                return(response);
+            },
+            transformErrorResponse: (response) => {
+                console.log(response.status)
+            },
+        }),
+
+        //delete existing product
+        deleteProduct: builder.mutation({
+            query: (productId) => ({
+                url:`api/pets/product/${productId}`,
+                method: "DELETE",
+            }),
+            transformResponse: (response) => {
+                console.log(response);
+                return response;
+            },
+            transformErrorResponse: (response) => {
+                console.log(response.status)
+            }
+        })
+    })
+})
+
+export default productsApi;
+export const {
+    useFetchProductsQuery,
+    useFetchSingleProductQuery,
+    useNewProductMutation,
+    useUpdateProductMutation,
+    useDeleteProductMutation
+} = productsApi
