44fd75aec4629d1eb62c0663ca82fc29591efcd0
divya-prav <divya.praveen.5215@gmail.com>
2023-11-26 14:43:28 -0600

    Added:checkout flow

--- client/redux/authApi.js
+++ client/redux/authApi.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
@@ -8,42 +7,62 @@ const authApi = createApi({
   }),
 
   endpoints: (builder) => ({
-
-      //user registration 
-      register: builder.mutation({
-        query: (user) => ({
-          url: "/auth/user/register",
-          method: "POST",
-          body: user,
-        }),
-        transformResponse: (response) => response,
-        transformErrorResponse: (response) =>response,
+    //user registration
+    register: builder.mutation({
+      query: (user) => ({
+        url: "/auth/user/register",
+        method: "POST",
+        body: user,
       }),
+      transformResponse: (response) => response,
+      transformErrorResponse: (response) => response,
+    }),
 
-      //user login
-      login: builder.mutation({
-        query: (user) => ({
-          url: "auth/user/login",
-          method: "POST",
-          body:user,
-        }),
-        transformResponse: (response) => {
-          console.log(response);
-          return response;
-        },
-        transformErrorResponse: (response) => {
-          console.log(response.status)
-        }
-      })
-
+    //userDetail Updation
+    updateUser: builder.mutation({
+      query: (userId, ...user) => ({
+        url: `/auth/user/register/${userId}`,
+        method: "PUT",
+        body: user,
       }),
+      transformResponse: (response) => response,
+      transformErrorResponse: (response) => response,
+    }),
 
-  
-}); 
+    //guest login
+    guestLogin: builder.mutation({
+      query: (user) => ({
+        url: "/auth/user/guest",
+        method: "POST",
+        body: user,
+      }),
+      transformResponse: (response) => response,
+      transformErrorResponse: (response) => response,
+    }),
 
-export const { 
+    //user login
+    login: builder.mutation({
+      query: (user) => ({
+        url: "auth/user/login",
+        method: "POST",
+        body: user,
+      }),
+      transformResponse: (response) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    }),
+  }),
+});
+
+export const {
   useRegisterMutation,
   useLoginMutation,
- } = authApi;
+  useGuestLoginMutation,
+  useUpdateUserMutation,
+} = authApi;
 
 export default authApi;
