4a40047ae03a7667f3be417c51f0dc608db43ec8
J’nae Gibbs <jnaegibbs@jnaes-air.lan>
2023-11-25 22:18:45 -0500

    cart updates

--- client/redux/cartApi.js
+++ client/redux/cartApi.js
@@ -14,13 +14,24 @@ const cartApi = createApi ({
         fetchCart: builder.query({
             query: () => `api/pets/cart`
         }),
+        fetchCartByUser: builder.query({
+            query: ({userId}) =>  `api/pets/cart/${userId}`
+        }),
 
+        createCart: builder.mutation({
+            query: (newCart) => ({
+              url: `api/pets/cart`,
+              method: "POST",
+              body: newCart,
+            })
+        })
     })
 
-})
+});
 
 
 export default cartApi;
 export const {
     useFetchCartQuery,
+    useCreateCartMutation
 } = cartApi
---
+++ client/redux/cartItemApi.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cartItemApi = createApi({
+  reducerPath: 'cartItemApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8080/',
+  }),
+  endpoints: (builder) => ({
+    fetchCartItems: builder.query({
+      query: () => 'api/pets/cartItem',
+    }),
+    createCartItem: builder.mutation({
+      query: (newCartItem) => ({
+        url: 'api/pets/cartItem',
+        method: 'POST',
+        body: newCartItem,
+      }),
+    }),
+    updateCartItem: builder.mutation({
+      query: ({ cartItemId, updatedCartItem }) => ({
+        url: `api/pets/cartItem/${cartItemId}`,
+        method: 'PUT',
+        body: updatedCartItem,
+      }),
+    }),
+    deleteCartItem: builder.mutation({
+      query: (cartItemId) => ({
+        url: `api/pets/cartItem/${cartItemId}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const {
+  useFetchCartItemsQuery,
+  useCreateCartItemMutation,
+  useUpdateCartItemMutation,
+  useDeleteCartItemMutation,
+} = cartItemApi;
+
+export default cartItemApi;
