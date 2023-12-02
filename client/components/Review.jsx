a9e5c6619ea18fc01a83c9420ac9548f29c4b84e
divya-prav <divya.praveen.5215@gmail.com>
2023-12-02 12:07:31 -0600

    Added: reviewApi in redux

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,4 +1,15 @@
+import { useGetReviewsQuery ,useGetSingleReviewQuery,useAddReviewMutation} from "../redux/reviewApi";
+
 const Review = () => {
+    const review = {
+        content:"testing review from review page",
+        rating : 4.5,
+        userId : 1,
+        productId : 3
+    }
+
+    const {data={},isloading,error} = useAddReviewMutation(review);
+    console.log(data);
     return(
         <div>Review Page</div>
     )
---
+++ client/redux/reviewApi.js
@@ -0,0 +1,77 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const reviewApi = createApi({
+  reducerPath: "reviewApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/",
+  }),
+
+  endpoints: (builder) => ({
+    //get all the reviews
+    getReviews: builder.query({
+      query: () => `api/pets/review`,
+    }),
+     // get a single review
+    getSingleReview: builder.query({
+      query: (reviewId) => `api/pets/review/${reviewId}`,
+    }),
+
+    //create a new review
+    addReview: builder.mutation({
+      query: (reviewDetails) => ({
+        url: `api/pets/review`,
+        method: "POST",
+        body: reviewDetails,
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    
+    }),
+
+    //update the existed review
+    updateReview: builder.mutation({
+      query: (reviewId, ...reviewDetails) => ({
+        url: `api/pets/review/${reviewId}`,
+        method: "PUT",
+        body: reviewDetails,
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    }),
+
+    //delete the existed review
+    deleteReview: builder.mutation({
+      query: (reviewId) => ({
+        url: `api/pets/review/${reviewId}`,
+        method: "DELETE",
+      }),
+      transformResponse: (response, meta, arg) => {
+        console.log(response);
+        return response;
+      },
+      transformErrorResponse: (response) => {
+        console.log(response.status);
+      },
+    }),
+  }),
+});
+
+export default reviewApi;
+export const {
+  useGetReviewsQuery,
+  useGetSingleReviewQuery,
+  useAddReviewMutation,
+  useUpdateReviewMutation,
+  useDeleteReviewMutation,
+} = reviewApi;
