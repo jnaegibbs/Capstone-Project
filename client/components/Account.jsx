626a35bd8166b9c347d474050486628df4dcbd6d
divya-prav <divya.praveen.5215@gmail.com>
2023-12-03 00:28:29 -0600

    Done-review page

--- client/components/Account.jsx
+++ client/components/Account.jsx
@@ -2,32 +2,75 @@ import React from "react";
 import { useAppSelector } from "../hooks";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Typography } from "@mui/material";
+import { Paper, Stack, Typography, Button } from "@mui/material";
 import Login from "./Login";
-import { selectUser } from "../redux/tokenSlice";
+import { useState } from "react";
+import { useFetchSingleProductQuery } from "../redux/productsApi";
+import { useNavigate } from "react-router-dom";
+
+const styles = {
+  mr: 2,
+  display: { xs: "none", md: "flex" },
+  fontFamily: "monospace",
+  fontWeight: 900,
+  fontSize: "large",
+  letterSpacing: ".1rem",
+  color: "inherit",
+  textDecoration: "none",
+  paddingLeft: 10,
+};
+
+const styles2 = {
+  width: 500,
+  maxWidth: "100%",
+  paddingLeft: 10,
+};
+
+const OrderHistory = ({ productId }) => {
+  const navigate = useNavigate();
+  const { data, error, isLoading } = useFetchSingleProductQuery(productId);
+  console.log("product data ---" + data);
+
+  return (
+    <Paper elevation={1} sx={{width:'80%',m:'1% 5%',padding:'20px'}}>
+    
+      <br/>
+      {data && (
+        
+        <Stack direction="row" spacing={2}>
+         
+          <Box sx={styles2}>
+            <img src={data.product.image} width="100px" height="100px" />
+          </Box>
+      
+          <Box sx={styles2}>
+            <Typography variant="h6">{data.product.name}</Typography>
+          </Box>
+          <Box sx={styles2}><Typography variant="h6">{data.product.price}</Typography></Box>
+          <Box>
+            <Button
+              variant="contained"
+              onClick={() => {
+                navigate(`/review/${productId}`);
+              }}
+            >
+              Review
+            </Button>
+          </Box>
+        </Stack>
+      )}
+      <br/>
+     
+    </Paper>
+  );
+};
 
 const Account = () => {
   const user = useAppSelector((state) => state.token.user);
- 
+  const [order, setOrder] = useState([user.order]);
+
   console.log("user:", user);
-
-  const styles = {
-    mr: 2,
-    display: { xs: "none", md: "flex" },
-    fontFamily: "monospace",
-    fontWeight: 900,
-    fontSize: "large",
-    letterSpacing: ".1rem",
-    color: "inherit",
-    textDecoration: "none",
-    paddingLeft: 10,
-  };
-
-  const styles2 = {
-    width: 500,
-    maxWidth: "100%",
-    paddingLeft: 10,
-  };
+  console.log("order:", order);
 
   return (
     <>
@@ -60,7 +103,7 @@ const Account = () => {
             <TextField
               fullWidth
               label="Email"
-             defaultValue={user.profile[0].email}
+              defaultValue={user.profile[0].email}
             />
           </Box>
           <br />
@@ -81,6 +124,16 @@ const Account = () => {
               defaultValue={user.profile[0].address}
             />
           </Box>
+          <br />
+          <br />
+          <Box>
+            <Typography sx={styles}> ORDER HISTORY</Typography>
+          </Box>
+          <br />
+          <br />
+          {order[0].map((order) => {
+            return <OrderHistory productId={order.productId} />;
+          })}
         </div>
       ) : (
         <div>
@@ -92,3 +145,4 @@ const Account = () => {
 };
 
 export default Account;
+export {OrderHistory}
--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,15 +1,36 @@
 import { useAddReviewMutation } from "../redux/reviewApi";
-import { Box, Typography, Rating, Button } from "@mui/material";
+import { Box, Typography, Rating, Button, Stack, Paper } from "@mui/material";
 import { useState } from "react";
-
+import { useParams } from "react-router-dom";
 import { TextareaAutosize } from "@mui/base";
 import { useAppSelector } from "../hooks";
+import Login from "./Login";
+import { useFetchSingleProductQuery } from "../redux/productsApi";
 
+const styles = {
+  mr: 2,
+  display: { xs: "none", md: "flex" },
+  fontFamily: "monospace",
+  fontWeight: 900,
+  fontSize: "large",
+  letterSpacing: ".1rem",
+  color: "inherit",
+  textDecoration: "none",
+  paddingLeft: 10,
+};
+const styles2 = {
+  width: 500,
+  maxWidth: "100%",
+  paddingLeft: 10,
+};
 const Review = () => {
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState("");
   const [createReview] = useAddReviewMutation();
   const user = useAppSelector((state) => state.token.user);
+  const { productId: productId } = useParams();
+  const { data, error, isLoading } = useFetchSingleProductQuery(productId);
+  console.log(productId);
 
   async function handleReviewSubmit() {
     try {
@@ -17,9 +38,11 @@ const Review = () => {
         rating,
         content,
         userId: user.id,
-        productId: 1,
+        productId: productId,
       });
       console.log(response);
+      setRating(0);
+      setContent("");
     } catch (error) {
       // Handle error, e.g., show an error message
       console.error("Error adding review:", error);
@@ -27,36 +50,83 @@ const Review = () => {
   }
 
   return (
-    <Box>
-    
-        <Typography component="h1">Create a Review</Typography>
+    <>
+      {user !== (null && undefined) ? (
+        <Box>
+          <br />
+          <br />
+          <Typography variant="h2" sx={styles}>
+            Create a Review
+          </Typography>
+          <br />
+          <Box sx={styles}>
+            <Paper elevation={0}>
+              {data && (
+                <Stack >
+                  <Box >
+                    <Typography variant="body1">{data.product.name}</Typography>
+                  </Box>
+                  <Box>
+                    <img
+                      src={data.product.image}
+                      width="100px"
+                      height="100px"
+                    />
+                  </Box>
 
-        <Typography component="h1">Rating</Typography>
-        <Rating
-          name="rating"
-          defaultValue={0}
-          precision={0.5}
-          value={rating}
-          size="large"
-          onChange={(event) => {
-            setRating(event.target.value);
-          }}
-        />
-        <Typography component="h1">Add Your Review</Typography>
-        <TextareaAutosize
-          placeholder="Write here..."
-          minRows={3}
-          value={content}
-          minLength={10}
-          onChange={(event) => setContent(event.target.value)}
-        />
-        <br />
-        <br />
-        <Button variant="contained" onClick={() => handleReviewSubmit()}>
-          Submit
-        </Button>
+                  
+                </Stack>
+              )}
+            </Paper>
+          </Box>
 
-    </Box>
+          <br />
+          <br />
+          <Typography variant="h2" sx={styles}>
+            Rating
+          </Typography>
+          <br />
+
+          <Rating
+            style={{ margin: "0 5%" }}
+            name="rating"
+            defaultValue={0}
+            precision={0.5}
+            value={rating}
+            size="large"
+            onChange={(event) => {
+              setRating(event.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <Typography variant="h2" sx={styles}>
+            Add Your Review
+          </Typography>
+          <br />
+
+          <TextareaAutosize
+            style={{ margin: "0 5%" }}
+            placeholder="Write here..."
+            minRows={5}
+            value={content}
+            minLength={10}
+            onChange={(event) => setContent(event.target.value)}
+          />
+          <br />
+          <br />
+          <Button
+            variant="contained"
+            style={{ margin: "0 5%" }}
+            onClick={() => handleReviewSubmit()}
+          >
+            Submit
+          </Button>
+        </Box>
+      ) : (
+        <Login />
+      )}
+    </>
   );
 };
 
