d11aca2e21453a257e0533cafa552a6d2d89f8f8
divya-prav <divya.praveen.5215@gmail.com>
2023-12-06 20:17:46 -0600

    new-migration

--- server/prisma/migrations/20231207021553_new_final/migration.sql
+++ server/prisma/migrations/20231207021553_new_final/migration.sql
@@ -2,18 +2,20 @@
 CREATE TYPE "productCategory" AS ENUM ('toy', 'clothes', 'food', 'accessories');
 
 -- CreateEnum
-CREATE TYPE "pets" AS ENUM ('dog', 'cat');
+CREATE TYPE "pets" AS ENUM ('dog', 'cat', 'smallPet');
 
 -- CreateEnum
 CREATE TYPE "role" AS ENUM ('admin', 'user');
 
+-- CreateEnum
+CREATE TYPE "status" AS ENUM ('active', 'archived');
+
 -- CreateTable
 CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "role" NOT NULL DEFAULT 'user',
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
@@ -22,8 +24,9 @@ CREATE TABLE "user" (
 CREATE TABLE "profile" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "age" INTEGER NOT NULL,
     "email" TEXT NOT NULL,
+    "phoneNumber" INTEGER NOT NULL,
+    "address" TEXT,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
@@ -63,6 +66,26 @@ CREATE TABLE "order" (
 );
 
 -- CreateTable
+CREATE TABLE "cart" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "cartStatus" "status" NOT NULL DEFAULT 'active',
+
+    CONSTRAINT "cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "cartItem" (
+    "id" SERIAL NOT NULL,
+    "itemAddedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "quantity" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "cartId" INTEGER NOT NULL,
+
+    CONSTRAINT "cartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "inventory" (
     "id" SERIAL NOT NULL,
     "productId" INTEGER NOT NULL,
@@ -71,8 +94,23 @@ CREATE TABLE "inventory" (
     CONSTRAINT "inventory_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "review" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
+    "postAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "review_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_id_key" ON "profile"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "profile_email_key" ON "profile"("email");
@@ -81,7 +119,25 @@ CREATE UNIQUE INDEX "profile_email_key" ON "profile"("email");
 CREATE UNIQUE INDEX "profile_userId_key" ON "profile"("userId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "product_id_key" ON "product"("id");
+
+-- CreateIndex
 CREATE INDEX "product_categoryName_idx" ON "product"("categoryName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "order_id_key" ON "order"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cart_id_key" ON "cart"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cartItem_id_key" ON "cartItem"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "inventory_id_key" ON "inventory"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "review_id_key" ON "review"("id");
 
 -- AddForeignKey
 ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
@@ -93,4 +149,19 @@ ALTER TABLE "order" ADD CONSTRAINT "order_productId_fkey" FOREIGN KEY ("productI
 ALTER TABLE "order" ADD CONSTRAINT "order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "cart" ADD CONSTRAINT "cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cartItem" ADD CONSTRAINT "cartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "cartItem" ADD CONSTRAINT "cartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "inventory" ADD CONSTRAINT "inventory_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "review" ADD CONSTRAINT "review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "review" ADD CONSTRAINT "review_productId_fkey" FOREIGN KEY ("productId") REFERENCES "product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
