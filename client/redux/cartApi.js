009a472c5745b97e7f7f94d10c42b6d31087be0b
divya-prav <divya.praveen.5215@gmail.com>
2023-12-08 15:54:34 -0600

    cartSlice added

--- client/redux/cartApi.js
+++ client/redux/cartApi.js
@@ -1,52 +1,45 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-//const getAuthToken = (state) => state.token.token;
 
+const cartApi = createApi({
+  reducerPath: "cartApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().token.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
 
-const cartApi = createApi ({
-    reducerPath: "cartApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "/",
-        prepareHeaders: (headers, {getState} )=> {
-            const token = getState().token.token
-            if (token) {
-                headers.set("authorization", `Bearer ${token}`)
-            }
-            return headers
-        }
-      
+  endpoints: (builder) => ({
+    //get all carts
+    fetchCart: builder.query({
+      query: () => `api/pets/cart`,
+    }),
+    fetchCartById: builder.query({
+      query: (cartId) => `api/pets/cart/${cartId}`,
     }),
 
+    fetchCartByUser: builder.query({
+      query: (userId) => `api/pets/cart/${userId}`,
+    }),
 
-    endpoints: (builder) => ({
-     
-        //get all carts
-        fetchCart: builder.query({
-            query: () => `api/pets/cart`
-        }),
-        fetchCartById: builder.query({
-            query: (cartId) =>  `api/pets/cart/${cartId}`,}),
-
-        fetchCartByUser: builder.query({
-            query: (userId) =>  `api/pets/cart/${userId}`,
-
-        }),
-
-        createCart: builder.mutation({
-            query: (newCart) => ({
-              url: `api/pets/cart`,
-              method: "POST",
-              body: newCart,
-            })
-        })
-    })
-
+    createCart: builder.mutation({
+      query: (newCart) => ({
+        url: `api/pets/cart`,
+        method: "POST",
+        body: newCart,
+      }),
+    }),
+  }),
 });
-
 
 export default cartApi;
 export const {
-    useFetchCartQuery,
-    useFetchCartByIdQuery,
-    useFetchCartByUserQuery,
-    useCreateCartMutation
-} = cartApi
+  useFetchCartQuery,
+  useFetchCartByIdQuery,
+  useFetchCartByUserQuery,
+  useCreateCartMutation,
+} = cartApi;
--- client/redux/cartSlice.js
+++ client/redux/cartSlice.js
@@ -1,21 +1,111 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import cartItemApi from "./cartItemApi";
+
+const initialState = {
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
 
 const cartSlice = createSlice({
-    name: "cart",
-     initialState: {
-        cartItems: {}
-      },
-      reducers: {
-        setCartItems: (state, action) => {
-          state.cartItems = action.payload;
-        },
-        addCartItem: (state, action) => {
-         state.cartItems.push(action.payload)
-          //state.cartItems =[...state.cartItems, action.payload];
-        },
-      },
-      
+  name: "cart",
+  initialState,
+  reducers: {
+    addCartItem: (state, action) => {
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (productIndex >= 0) {
+        //Item already exist in the cart
+        //Increase the cart quantity
+        state.cartItems[productIndex].cartQuantity += 1;
+        toast.success(`${action.payload.name} increased by one`, {
+          position: "top-left",
+        });
+      } else {
+        //Item doesn't exists in the cart
+        //Add item to the cart
+        const tempProduct = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProduct);
+        toast.success(`${action.payload.name} Product added to cart`, {
+          position: "top-left",
+        });
+      }
+      //save cart to Local stroage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    decreaseQuantity: (state, action) => {
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (state.cartItems[productIndex].cartQuantity > 1) {
+        state.cartItems[productIndex].cartQuantity -= 1;
+      } else if (state.cartItems[productIndex].cartQuantity === 1) {
+        const newCartItem = state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.cartItems = newCartItem;
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    removeItemFromCart: (state, action) => {
+      const newCartProduct = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.cartItems = newCartProduct;
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    clearCart: (state, action) => {
+      state.cartItems = [];
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    calculateSubtotal: (state, action) => {
+      const array = [];
+      state.cartItems.length !== 0 &&
+        state.cartItems.map((item) => {
+          const { price, cartQuantity } = item;
+          const cartItemAmount = Number(price.substring(1)) * cartQuantity;
+          return array.push(cartItemAmount);
+        });
+      const totalAmount = array.reduce((sum, curr) => sum + curr);
+      state.cartTotalAmount = totalAmount;
+    },
+    calculateQuantity: (state, action) => {
+      const array = [];
+      state.cartItems.map((item) => array.push(item.cartQuantity));
+      const totalQuantity = array.reduce((sum, curr) => sum + curr);
+      state.cartTotalQuantity = totalQuantity;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      cartItemApi.endpoints.fetchCartItemByUserCart.matchFulfilled,
+      (state, payload) => {
+        console.log("cartSlice   "+payload);
+        localStorage.setItem("cartItems", JSON.stringify(payload.product));
+      }
+    );
+
+    builder.addMatcher(
+      cartItemApi.endpoints.deleteAllCartItem.matchFulfilled,
+      (state,payload)=> localStorage.setItem('cartItems',[])
+    )
+ },
 });
 
 export default cartSlice.reducer;
-export const {setCartItems, addCartItem } = cartSlice.actions;
+export const selectCartItems = (state) => state.cart.cartItems;
+export const selectCartTotalQuantity = (state) => state.cart.cartTotalQuantity;
+export const selectCartTotalAmount = (state) => state.cart.cartTotalAmount;
+export const {
+  addCartItem,
+  decreaseQuantity,
+  removeItemFromCart,
+  clearCart,
+  calculateSubtotal,
+  calculateQuantity,
+} = cartSlice.actions;
