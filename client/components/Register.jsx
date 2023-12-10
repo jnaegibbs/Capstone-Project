9b7993223e48a9f6086bead581f3ff392866b096
Katie Barrett <kayt.barrett22@gmail.com>
2023-12-10 11:35:02 -0500

    small bug fixes made

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -23,13 +23,15 @@ const Register = () => {
   const submitRegister = async (event) => {
     event.preventDefault();
 
+    const sanitizedPhone = phone.replace(/\D/g, '');
+
     try{
       const response = await register({
         username: username,
         password: password,
         name: name,
         email: email,
-        phone: phone,
+        phone: sanitizedPhone,
         address: address,
       })
       console.log("RESPONSE", response)
@@ -132,8 +134,8 @@ const Register = () => {
           <TextField
             required
             label="Mobile Number"
-            type="number"
-            placeholder="xxxxxxxxxx"
+            type="text"
+            placeholder="XXX-XXX-XXXX"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             fullWidth
